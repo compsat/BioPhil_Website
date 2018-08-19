2cecfd9d13b7bbe441ecb8829e8854c8b5bdd256
Daniel <daniel.delrio@obf.ateneo.edu>
2018-08-19 15:06:34 +0800

    Fix NameError for _

--- bio_phil/webapp/models.py
+++ bio_phil/webapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
 USER_TYPE = (
 	('Teacher', 'Teacher'),
