96a1d81d8c1ffd01566ad14ee0a8fe96dcf8b8da
Daniel <daniel.delrio@obf.ateneo.edu>
2018-09-15 22:13:20 +0800

    Add routes for submission views

--- bio_phil/webapp/urls.py
+++ bio_phil/webapp/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('generate/', views.generate_access_codes, name='generate_access_codes'),
     path('manage/', views.manage_access_codes, name='manage_access_codes'),
+    path('submit/', login_required(views.SubmitAnswer.as_view(template_name='webapp/submit.html')), name='submit'),
+    path('submissions/', login_required(views.SubmissionList.as_view(template_name='webapp/submissions_list.html')), name='submissions_list'),
+    path('submissions/<int:pk>/', login_required(views.EditAnswer.as_view()), name='edit')
 ]
