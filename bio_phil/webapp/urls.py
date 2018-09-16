3ea65e6636433543050b50042188e11a0627f400
Miguel Habana <mighabana@gmail.com>
2018-09-16 23:04:13 +0800

    Fix issues with urlpatterns and update media path

--- bio_phil/webapp/urls.py
+++ bio_phil/webapp/urls.py
@@ -12,8 +12,6 @@ urlpatterns = [
     path('generate/', views.generate_access_codes, name='generate_access_codes'),
     path('manage/', views.manage_access_codes, name='manage_access_codes'),
     path('img_test',views.images,name ='image_tester'),
-    staticfiles_urlpatterns(),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     path('submit/', login_required(views.SubmitAnswer.as_view(template_name='webapp/submit.html')), name='submit'),
     path('submissions/', login_required(views.SubmissionList.as_view(template_name='webapp/submissions_list.html')), name='submissions_list'),
     path('submissions/<int:pk>/', login_required(views.EditAnswer.as_view()), name='edit')
