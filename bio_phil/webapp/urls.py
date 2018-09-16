f587e35f5c10f48c84398fc0bf6f7b167c0f45c2
LienLeinad <neil.bau@tista.net>
2018-09-16 21:26:58 +0800

    Made final fixes on URL code

--- bio_phil/webapp/urls.py
+++ bio_phil/webapp/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('generate/', views.generate_access_codes, name='generate_access_codes'),
     path('manage/', views.manage_access_codes, name='manage_access_codes'),
-    path('img_test',views.images,name ='image_tester')
-]
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [
+    path('img_test',views.images,name ='image_tester'),
+    staticfiles_urlpatterns(),
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     path('submit/', login_required(views.SubmitAnswer.as_view(template_name='webapp/submit.html')), name='submit'),
     path('submissions/', login_required(views.SubmissionList.as_view(template_name='webapp/submissions_list.html')), name='submissions_list'),
     path('submissions/<int:pk>/', login_required(views.EditAnswer.as_view()), name='edit')
 ]
+
+
