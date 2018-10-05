4b1901851bff6926add6826f27cd3bdf5ced4d92
Daniel <daniel.delrio@obf.ateneo.edu>
2018-10-05 23:45:27 +0800

    Make modules view login_required

--- bio_phil/webapp/urls.py
+++ bio_phil/webapp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('submit/', login_required(views.SubmitAnswer.as_view(template_name='webapp/submit.html')), name='submit'),
     path('submissions/', login_required(views.SubmissionList.as_view(template_name='webapp/submissions_list.html')), name='submissions_list'),
     path('submissions/<int:pk>/', login_required(views.EditAnswer.as_view()), name='edit'),
-    path('modules/', views.module, name='module_tester')
+    path('modules/', login_required(views.module), name='module_tester')
 ]
 
 
