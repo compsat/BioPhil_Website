dda3b76c815fb6381a62fe664216ea169dc33200
Daniel <daniel.delrio@obf.ateneo.edu>
2018-10-31 08:55:48 +0800

    Remove context_processor

--- bio_phil/bio_phil/settings.py
+++ bio_phil/bio_phil/settings.py
@@ -61,7 +61,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'webapp.context_processors.include_login_form'
             ],
         },
     },
