09084c6f032a3a4cd585775a4669eebbf0ff0bd1
Daniel <daniel.delrio@obf.ateneo.edu>
2020-09-22 00:24:54 +0800

    Integrate cloudinary and Dropbox

--- bio_phil/bio_phil/production_settings.py
+++ bio_phil/bio_phil/production_settings.py
@@ -8,7 +8,7 @@ from decouple import config
 
 DEBUG = config('DEBUG', cast=bool)
 basepath = '/kunden/homepages/41/d77006110/htdocs'
-STATIC_ROOT = basepath + 'BIOPHIL/BioPhil_Website/bio_phil/webapp/static'
+# STATIC_ROOT = basepath + 'BIOPHIL/BioPhil_Website/bio_phil/webapp/static'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -30,33 +30,35 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 SECURE_HSTS_PRELOAD = True
 
-INSTALLED_APPS = [
-    'webapp.apps.WebappConfig',
-    'background_task',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'collectfast',
-    'django.contrib.staticfiles',
-    'storages',
-]
+# INSTALLED_APPS = [
+#     'webapp.apps.WebappConfig',
+#     'background_task',
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     # 'collectfast',
+#     'django.contrib.staticfiles',
+#     'storages',
+#     'cloudinary_storage',
+#     'cloudinary',
+# ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    },
-    'collectfast': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'collectfast_cache',
-        'TIMEOUT': 60,
-        'OPTIONS': {
-            'MAX_ENTRIES': 10000
-        },
-    },
-}
-COLLECTFAST_CACHE = 'collectfast'
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     },
+#     'collectfast': {
+#         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+#         'LOCATION': 'collectfast_cache',
+#         'TIMEOUT': 60,
+#         'OPTIONS': {
+#             'MAX_ENTRIES': 10000
+#         },
+#     },
+# }
+# COLLECTFAST_CACHE = 'collectfast'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
