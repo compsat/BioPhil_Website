4f1a53b431bdb0ca112de34c9dcd4f9dbe19f5fa
104447 <daniel.delrio@obf.ateneo.edu>
2020-09-21 19:07:28 +0200

    Change emails

--- bio_phil/webapp/urls.py
+++ bio_phil/webapp/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_DIR, document_root=settings.MEDIA_ROOT)
